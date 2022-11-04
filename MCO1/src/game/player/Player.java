147dcc9e0fe327b44ea97e236003d77eaf259fb0
Brett Harley Mider <brett_mider@dlsu.edu.ph>
2022-11-04 17:06:17 +0800

    Added documentation for Player class

--- MCO1/src/game/player/Player.java
+++ MCO1/src/game/player/Player.java
@@ -2,6 +2,10 @@ package game.player;
 
 import game.farm.*;
 
+/**
+ * Represents the player which holds the farmer type/rank they have, their
+ * farm lot, money, level, experience, and other tracked statistics.
+ */
 public class Player {
     private Farmer farmer;
     private FarmLot farmLot;
@@ -10,9 +14,21 @@ public class Player {
     private int level;
     private double experience;
 
+    /**
+     * Creates a player.
+     */
     public Player() {
     }
     
+    /**
+     * Creates a player and initializes it using the given farmer type, money
+     * count, level, and experience. The rest are initialized automatically.
+     * @param farmer
+     * @param objectCoins
+     * @param level
+     * @param experience
+     * @see FarmerType
+     */
     public Player(FarmerType farmer, double objectCoins, int level, double experience) {
         this.farmer = new Farmer(farmer);
         this.farmLot = new FarmLot();
@@ -22,6 +38,19 @@ public class Player {
         this.experience = experience;
     }
 
+    /**
+     * Creates a player and initializes it using the given farmer type, farm
+     * lot, money count, stats, level, and experience.
+     * @param farmer        The type/rank of farmer the player has.
+     * @param farmLot       The farm lot (collection of tles) the player has.
+     * @param objectCoins   Initial money count.
+     * @param stats         A collection of tracked statistics.
+     * @param level         Initial starting level.
+     * @param experience    Initial experience.
+     * @see FarmerType
+     * @see FarmLot
+     * @see Stats
+     */
     public Player(Farmer farmer, FarmLot farmLot, double objectCoins, Stats stats, int level, double experience) {
         this.farmer = farmer;
         this.farmLot = farmLot;
@@ -31,6 +60,17 @@ public class Player {
         this.experience = experience;
     }
 
+    /**
+     * Registers the player for a given type/rank. The stats of the crops
+     * in the field are externally updated when the player
+     * {@link app.Application#openRegisterMenu() registers for a rank}.
+     * Newly bought crops' stats are updated when the player
+     * {@link app.Application#openBuyCropMenu() buys a crop}.
+     * @param farmerType   The type/rank to be registered as.
+     * @return A report containing details about the registration.
+     * @see app.Application Application
+     * @see RankRegistrationReport
+     */
     public RankRegistrationReport registerRank(FarmerType farmerType) {
         RankRegistrationReport report = new RankRegistrationReport();
 
@@ -47,9 +87,11 @@ public class Player {
         } else if (objectCoins < rankRegFee) {
             report.setMessage(RankRegistrationReport.REGISTER_NO_MONEY);
         } else {
-            // the stats of the crops in the field are updated externally (see Application class)
+            // the stats of the crops in the field are updated externally
             report.setSuccess(true);
             report.setMessage(RankRegistrationReport.REGISTER_SUCCESS);
+
+            // update player's perks
             report.setNewRank(farmerType);
             report.setNewBonusEarnings(farmerType.getBonusEarnings());
             report.setNewSeedCostReduction(farmerType.getSeedCostReduction());
@@ -61,24 +103,47 @@ public class Player {
         return report;
     }
 
+    /**
+     * Adds money to the player's money count.
+     * @param amount   The amount of money to be added.
+     * @return The new money count of the player.
+     */
     public double addMoney(double amount) {
         return objectCoins += amount;
     }
 
+    /**
+     * Deducts money from the player's money count.
+     * @param amount   The amount of money to be deducted.
+     * @return The new money count of the player.
+     */
     public double deductMoney(double amount) {
         return objectCoins -= amount;
     }
 
+    /**
+     * Updates the level of the player based on their experience.
+     * @return The updated level of the player.
+     */
     public int updateLevel() {
         return level = (int)(experience / 100);
     }
     
+    /**
+     * Adds experience to the player and updates their level.
+     * @param amount   The amount of experience to be added.
+     * @return The new amount of experience the player has.
+     */
     public double addXP(double amount) {
         experience += amount;
         updateLevel();
         return experience;
     }
     
+    /**
+     * Prints the state of the player, including their current money count,
+     * level, and experience.
+     */
     public void printState() {
         var s = "Objectcoins: " + objectCoins + "\n"
               + "Level: " + level + "\n"
