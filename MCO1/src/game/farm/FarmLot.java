159d6971f6c256d5288913a767fb19ee086a9f99
Brett Harley Mider <brett_mider@dlsu.edu.ph>
2022-11-04 16:25:59 +0800

    Added documentation for the FarmLot class

--- MCO1/src/game/farm/FarmLot.java
+++ MCO1/src/game/farm/FarmLot.java
@@ -2,13 +2,25 @@ package game.farm;
 
 import game.tile.Tile;
 
+/**
+ * Holds the tiles that the players will be working on.
+ */
 public class FarmLot {
     private Tile tiles; // TODO: in MCO2, update to some data structure later for multiple tiles
     
+    /**
+     * Creates a farm lot and initializes it with a new Tile.
+     * @see Tile
+     */
     public FarmLot() {
         this.tiles = new Tile();
     }
 
+    /**
+     * Creates a farm lot and initializes it with the given Tile.
+     * @see Tile
+     * @param tiles
+     */
     public FarmLot(Tile tiles) {
         this.tiles = tiles;
     }
