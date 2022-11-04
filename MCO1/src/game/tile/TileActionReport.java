63210ed55c9c69daa6fe51d06ddb6c605593e803
Brett Harley Mider <brett_mider@dlsu.edu.ph>
2022-11-04 21:04:15 +0800

    Added documentation for TileActionReport class

--- MCO1/src/game/tile/TileActionReport.java
+++ MCO1/src/game/tile/TileActionReport.java
@@ -1,19 +1,36 @@
 package game.tile;
 
+/**
+ * Contains feedback for a tile action/option.
+ */
 public class TileActionReport {
     private boolean success;
     private String message;
     private double expGained;
 
+    /**
+     * Creates a tile action report.
+     */
     public TileActionReport() {
     }
 
+    /**
+     * Creates a tile action report and initializes its success state,
+     * feedback message, and gained experience.
+     * @param success     Whether the action was a success or not.
+     * @param message     Feedback message for performing the action.
+     * @param expGained   Experience gained for performing the action.
+     */
     public TileActionReport(boolean success, String message, double expGained) {
         this.success = success;
         this.message = message;
         this.expGained = expGained;
     }
 
+    /**
+     * Returns a string of the report's details, including its success state
+     * and feedback message.
+     */
     @Override
 	public String toString() {
         var s = "Report:\n"
